6f271884defed7bba958d5d4f902a8be1032d018
Shagil <shagilsid@gmail.com>
2022-01-15 07:31:00 +0530

    Can sum with DP

--- out/production/DynamicProgramming/memoization/canSum/readme.md
+++ out/production/DynamicProgramming/memoization/canSum/readme.md
@@ -9,9 +9,5 @@ n=array length
 Height of tree= `m` where m can be decreased by 1 in worst case
 Branching factor=`n` where n is the number of options a number can take.
 - Without DP
-  - T.C.:`O(n^m)` max number of branches a node can have ^ height of tree.
-  - Here number of branches a node can have = number of elements in array.
+  - T.C.:`O(n^m)`
   - S.C.: `O(m)`
-- With DP
-  - T.C. `O(n*m)`: A target number can be operated using all numbers present in an array. Only 1 time this operation is required for particular target sum.
-  - S.C: `O(m)`
--- src/memoization/canSum/Problem.java
+++ src/memoization/canSum/Problem.java
@@ -1,5 +1,8 @@
 package memoization.canSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
     static boolean canSum(int target,int[] numbers){
         if(target==0)
@@ -16,6 +19,27 @@ public class Problem {
     }
 
     public static void main(String[] args) {
-        System.out.println(canSum(300,new int[]{7,14}));
+//        System.out.println(canSum(300,new int[]{7,14}));
+        Map<Integer,Boolean> memo=new HashMap<>();
+        System.out.println(canSumDP(600,new int[]{7,14},memo));
+    }
+
+    static boolean canSumDP(int target, int[] numbers, Map<Integer,Boolean> memo){
+        if (memo.containsKey(target)){
+            return memo.get(target);
+        }
+        if(target==0)
+            return true;
+        if(target<0)
+            return false;
+        for (int i = 0; i < numbers.length; i++) {
+            boolean returnValue=canSum(target-numbers[i],numbers);
+            if (returnValue) {
+                memo.put(target,true);
+                return true;
+            }
+        }
+        memo.put(target,false);
+        return false;
     }
 }
