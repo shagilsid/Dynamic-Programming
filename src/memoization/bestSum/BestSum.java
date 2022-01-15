dad0e5ea3fd8666b7bc585d70fa9bdc05c4b1442
Shagil <shagilsid@gmail.com>
2022-01-15 11:51:33 +0530

    Best Sum with DP

---
+++ src/memoization/bestSum/BestSum.java
@@ -0,0 +1,64 @@
+package memoization.bestSum;
+
+import java.util.*;
+
+public class BestSum {
+
+    static List<Integer> bestSum(int targetSum, int[] elements){
+        if (targetSum==0)
+            return new ArrayList<>();
+        if (targetSum<=0)
+            return null;
+        List<Integer> bestAnswer=null;
+        for (int number :
+                elements) {
+            int remainder = targetSum - number;
+            List<Integer> answer=bestSum(remainder,elements);
+            if (answer!=null){
+                answer.add(number);
+               if (bestAnswer==null||bestAnswer.size()> answer.size()){
+                   bestAnswer=answer;
+               }
+            }
+        }
+        return bestAnswer;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(bestSum(7,new int[]{2,4}));
+//        System.out.println(bestSum(8,new int[]{2,3,5}));
+        System.out.println(bestSum(8,new int[]{1,4,5}));
+//        System.out.println(bestSum(100,new int[]{1,2,5,25}));
+        Map<Integer,List<Integer>> memo=new HashMap<>();
+//        System.out.println(bestSumDP(100,new int[]{1,2,5,25},memo));
+//        System.out.println(bestSumDP(7,new int[]{2,4},memo));
+//        System.out.println(bestSumDP(8,new int[]{2,3,5},memo));
+        System.out.println(bestSumDP(22,new int[]{6,2,8,4,5},memo));
+
+    }
+//Not working fine
+    static List<Integer> bestSumDP(int targetSum, int[] elements, Map<Integer,List<Integer>> memo){
+        if (memo.containsKey(targetSum))
+            return memo.get(targetSum);
+        if (targetSum==0)
+            return new ArrayList<>();
+        if (targetSum<=0)
+            return null;
+        List<Integer> bestAnswer=null;
+        for (int number :
+                elements) {
+            int remainder = targetSum - number;
+            List<Integer> answer=bestSumDP(remainder,elements,memo);
+            if (answer!=null){
+                answer.add(number);
+                if (bestAnswer==null||bestAnswer.size()> answer.size()){
+                    bestAnswer=answer;
+                }
+            }else{
+                memo.put(remainder,null);
+            }
+        }
+        memo.put(targetSum,bestAnswer==null?null:new ArrayList<>(bestAnswer));
+        return bestAnswer;
+    }
+}
--- src/memoization/bestSum/readme.md
+++ src/memoization/bestSum/readme.md
@@ -1,9 +1,9 @@
 #Problem
-`int[] howSum(targetSum, numbers[]);`: Retrieve an array containing any combination of elements that add up to exactly the target sum.
+`int[] bestSum(targetSum, numbers[]);`: Retrieve an array containing the shortest combination of elements that add up to exactly the target sum.
 - You may use an element of the array as many times as needed.
 - assume all input numbers are non negative.
 - if not present return null.
-- if multiple present, return any one.
+- if there is a tie present, return any one.
 
 #Time Complexity
 m=target sum
@@ -11,8 +11,8 @@ n=array length
 Height of tree= `m` where m can be decreased by 1 in worst case
 Branching factor=`n` where n is the number of options a number can take.
 - Without DP:
-  - T.C.: `O(n^m)`
-  - S.C.: `O(m)`
+    - T.C.: `O(n^m)`
+    - S.C.: `O(m)`
 - With DP:
-  - T.C: `O(m*n)`
-  - S.C.: `O(m*m)`The extra *m is due to the space taken by an list of elements into memo object
+    - T.C: `O(m*n)`
+    - S.C.: `O(m*m)`The extra *m is due to the space taken by an list of elements into memo object
